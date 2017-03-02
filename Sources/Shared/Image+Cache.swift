03ea5d6e62085f622726262db3418b4999257fd2
Christoffer Winterkvist <christoffer@winterkvist.com>
2017-03-02 12:05:26 +0100

    Remove memorySize

--- Sources/Shared/Image+Cache.swift
+++ Sources/Shared/Image+Cache.swift
@@ -8,12 +8,4 @@ extension Image {
   public static func decode(_ data: Data) -> CacheType? {
     return Decompressor.decompress(data as Data)
   }
-
-  public var memorySize: Int {
-    guard let cgImage = cgImage else {
-      return 0
-    }
-
-    return cgImage.height * cgImage.bytesPerRow
-  }
 }
